ef26107b9c5408e38dfad962987eaa5737a6091f
Luciano2244 <64085517+Luciano2244@users.noreply.github.com>
2024-05-09 20:38:57 -0300

    Add files via upload

---
+++ Programacion/Teorico/ejercicio4.py
@@ -0,0 +1,7 @@
+"""Se quiere definir una clase que permita controlar un sintonizador digital de emisoras
+FM; concretamente, lo que se desea es dotar al controlador de una interfaz que
+permita subir (up) o bajar (down) la frecuencia (en saltos de 0.5 MHz) y mostrar la
+frecuencia sintonizada en un momento dado (display).
+Supondremos que el rango de frecuencias a manejar oscila entre los 80 Mhz y los
+108 MHz y que al inicio, el controlador sintoniza a 80 MHz.
+Si durante una operación de subida o bajada se sobrepasa uno de"""

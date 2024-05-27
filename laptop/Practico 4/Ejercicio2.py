29930f51f55294c22801a8de8097990591b989da
Luciano2244 <64085517+Luciano2244@users.noreply.github.com>
2024-05-27 15:08:31 -0300

    Delete laptop directory

--- laptop/Practico 4/Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-def crear_matriz(matrix):
-    return matrix
-def imprimir_matriz(matriz):
-    for i in range(0,2):
-        for j in range(0,5):
-            print(matriz[i][j], end=" ")
-        print("")
